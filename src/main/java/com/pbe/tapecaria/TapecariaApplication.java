cf1536519018f4361d98f5c0ba83fcc129e2c5d8
empirelk <gustaagostinho1@gmail.com>
2026-02-09 13:45:36 -0300

    projeto spring boot com java tapecaria

---
+++ src/main/java/com/pbe/tapecaria/TapecariaApplication.java
@@ -0,0 +1,13 @@
+package com.pbe.tapecaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapecariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapecariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbe/tapecaria/TapecariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbe.tapecaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapecariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
